7edaeed65d9c998d72e05366cc497eda5ec4bf23
AbhayaStha <69980750+AbhayaStha@users.noreply.github.com>
2020-12-17 20:53:01 +0545

    Add files via upload

---
+++ src/bankManagement/conn.java
@@ -0,0 +1,18 @@
+package bankManagement;
+
+import java.sql.*;
+
+public class conn {
+    Connection c;
+    Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///project1?autoReconnect=true&useSSL=false","root", "");
+            s = c.createStatement();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
